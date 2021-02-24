6cddfc14dcfe033de0cb74e8d51d722b632b9962
João Vitor <joaovitorbaroni@outlook.com>
2021-02-24 07:12:32 -0300

    Novo método adicionado
    Adicionado novo model para detalhes da rota
    Nova função adicionada em HelperMethods

---
+++ lib/model/direction_details.dart
@@ -0,0 +1,11 @@
+class DirectionDetails{
+  String distanceText;
+  String durationText;
+  int distanceValue;
+  int durationValue;
+  String encodedPoints; //drawer no mapa
+
+  DirectionDetails({this.distanceText, this.durationText, this.distanceValue, this.durationValue, this.encodedPoints});
+
+
+}
--- lib/utils/request_helper_methods.dart
+++ lib/utils/request_helper_methods.dart
@@ -2,18 +2,20 @@ import 'package:connectivity/connectivity.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:rider/components/progress_dialog.dart';
 import 'package:rider/constants/constants.dart';
 import 'package:rider/model/address.dart';
+import 'package:rider/model/direction_details.dart';
 import 'package:rider/model/prediction.dart';
 import 'package:rider/provider/app_data.dart';
 import 'package:rider/utils/request_helper.dart';
 import 'package:sprintf/sprintf.dart';
 
 class HelperMethods {
-  static Future<String> findCordinateAddress(
-      Position position, BuildContext context) async {
+
+  static Future<String> findCordinateAddress(Position position, BuildContext context) async {
     String placeAddress = '';
     final String urlGoogleMaps = sprintf(
         'https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=%s',
@@ -42,7 +44,6 @@ class HelperMethods {
     }
     return placeAddress;
   }
-
   static Future<List<Prediction>> searchPlace(String placeName) async {
     if (placeName.length > 0) {
       List<Prediction> destinationList = [];
@@ -68,13 +69,11 @@ class HelperMethods {
       return [];
     }
   }
-
-  static void setDestionationDetails(String placeID, BuildContext context) async {
+  static Future<void> setDestionationDetails(String placeID, BuildContext context) async {
 
     showDialog(context: context, builder: (context) => ProgressDialog(status: 'Please wait...',),);
     
-    final String urlGoogleDetails =
-        'https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&key=${API_KEY}';
+    final String urlGoogleDetails = 'https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&key=${API_KEY}';
     var response = await RequestHelper.getRequest(urlGoogleDetails);
 
     if (response == 'error') {
@@ -94,4 +93,24 @@ class HelperMethods {
 
     Navigator.pop(context);
   }
+
+
+  static Future<DirectionDetails> getDirectionDetails(LatLng pickupPosition, LatLng destinationPosition) async{
+    final String urlGoogleDirection = 'https://maps.googleapis.com/maps/api/directions/json?origin=${pickupPosition.latitude},${pickupPosition.longitude}&destination=${destinationPosition.latitude},${destinationPosition.longitude}&mode=driving&key=${API_KEY}';
+    var response = await RequestHelper.getRequest(urlGoogleDirection);
+
+    if(response == 'error'){
+      return null;
+    }
+
+    DirectionDetails details = DirectionDetails();
+    details.durationText  = response['routes'][0]['legs'][0]['duration']['text'];
+    details.durationValue = response['routes'][0]['legs'][0]['duration']['value'];
+    details.distanceValue = response['routes'][0]['legs'][0]['distance']['value'];
+    details.distanceText  = response['routes'][0]['legs'][0]['distance']['text'];
+    details.encodedPoints = response['routes'][0]['overview_polyline']['points'];
+    return details;
+
+  }
+
 }
